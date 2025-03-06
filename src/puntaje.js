95498c66c7e58e2b60dc3a1d338b70259d3b4d17
jarPunk <jar_ax@hotmail.com>
2025-03-06 09:36:52 -0400

    el score este gando con 15 - 0

--- src/puntaje.js
+++ src/puntaje.js
@@ -1,5 +1,9 @@
-function puntuacion() {
-        return "love,love";
+function obtenerScore(score1,score2) {
+    if(score1 == 1 && score2 == 0)
+    {
+        return "15 - 0"
+    }
+    return "love,love";
   }
-  export default puntuacion;
+  export default obtenerScore;
   
--- src/puntaje.test.js
+++ src/puntaje.test.js
@@ -1,6 +1,10 @@
-import puntuacion from "./puntaje.js";
+import obtenerScore from "./puntaje.js";
 describe("Marcador de puntaje de juego", () => {
-it("en caso de qeu ambos jugadores tengas 0 el marcador es love love", () => {
-    expect(puntuacion(0,0)).toEqual("love,love");
+it("en caso de que ambos jugadores tengas 0 el marcador es love love", () => {
+    expect(obtenerScore(0,0)).toEqual("love,love");
+  });
+  it("en caso de que el score1 este ganando con 15 - 0 ", () => {
+    expect(obtenerScore(1,0)).toEqual("15 - 0");
   });
 });
+
